4d305c4af1b47f3fddf677a6ce7945eb04ffe265
巫镇泉 <zhenquan.me@gmail.com>
2023-04-02 03:30:38 +0000

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 
-function App() {
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { MenuDropdownHeadless } from './drop-down';
 
+const routerConfig = {
+  menuDropdownHeadless: {
+    path: "/MenuDropdownHeadless",
+    element: <MenuDropdownHeadless onSelect={() => { }} items={[
+      { name: 'Vue' },
+      { name: 'Svelte' },
+      { name: 'React' },
+      { name: 'Angular' },
+    ]} />,
+  }
+}
+
+function Index() {
   return (
-    <div className="App">
-      <div>Headless UI</div>
+    <div>
+      <Link to={`contacts/1`}>Your Name</Link>
     </div>
   )
 }
 
-export default App
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>
+        <Link to={routerConfig.menuDropdownHeadless.path}>{routerConfig.menuDropdownHeadless.path}</Link>
+      </div>
+    },
+    ...Object.values(routerConfig),
+  ]);
+
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
